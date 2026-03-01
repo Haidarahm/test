b220d9b5e94f4e81b74d517b8f360bfc2d0df467
Haidar Ahmad <haidara.ahmad@tikit.ae>
2026-03-01 14:52:59 +0300

    Update Home component layout: Replaced "Hello World" with a styled title and added sections for content and technology, enhancing the overall structure and design.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <section className="emigo-container h-screen flex flex-col ">
+        <h1 className="title h-fit font-ibrand uppercase font-normal  text-[80px] w-full text-center leading-[100%] ">shop <br/> emigo <br/> container</h1>
+        <div className="cards flex flex-1 w-full bg-red-400">
+
+          
+        </div>
+      </section>
+      <section className="emigo-technology h-screen"></section>
     </main>
   );
 }
